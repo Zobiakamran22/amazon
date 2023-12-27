71f1d6f2c80ef09c02a059dbbf0b87834d7dbe58
Zobiakamran22 <zobiakamran47@gmail.com>
2023-12-27 15:12:34 +0500

    Amazon

---
+++ Assets/js/app.js
@@ -0,0 +1,31 @@
+let x = document.getElementById("x");
+let dropdown = document.getElementById('dropdown');
+
+
+
+
+
+// let headertwo= document.getElementById("header-two");
+
+
+
+
+
+
+
+x.addEventListener ('click', function () {
+    dropdown.style.display = 'block'
+})
+    
+x.addEventListener ('mouseover', function () {
+    dropdown.style.display = 'none'
+})
+    
+
+// x.addEventListener ('click', function () {
+//     headertwo.style.backgroundColor = 'blue'
+// })
+    
+
+
+
